98a15c0d417caed3d0d14c601438ddcd521e0dd4
DogeRam1500 <82404846+DogeRam1500@users.noreply.github.com>
2023-05-04 19:35:57 -0400

    Update posttweet.py

--- posttweet.py
+++ posttweet.py
@@ -2,6 +2,6 @@ import tweepy
 auth = tweepy.OAuthHandler('CONSUMER KEY', 'CONSUMER SECRET')
 auth.set_access_token('ACCESS TOKEN', 'ACCESS TOKEN SECRET')
 api=tweepy.API(auth, wait_on_rate_limit=True)
-f=open('PATH TO TWEET FILE', 'r')
+f=open('/PATH/TO/TWEET/FILE', 'r')
 api.update_status(f.read())
 f.close()
