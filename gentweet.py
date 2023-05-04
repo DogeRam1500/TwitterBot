209a6bc92f7d783dc0d9bb71d281fc62d8076beb
DogeRam1500 <82404846+DogeRam1500@users.noreply.github.com>
2023-05-04 19:33:15 -0400

    Update gentweet.py

--- gentweet.py
+++ gentweet.py
@@ -1,5 +1,5 @@
-f=open('PATH TO CONDITIONS FILE', 'r')
-tweet=open('PATH TO WRITE TWEET TO', 'w')
+f=open('/home/pi/consolewdfiles/twitter.txt', 'r')
+tweet=open('/home/pi/tweet.txt', 'w')
 lines=f.readlines()
 variables=[]
 for i in lines:
@@ -27,24 +27,25 @@ soiltemp2=variables[19]
 soiltemp3=variables[20]
 soiltemp4=variables[21]
 pooltemp=variables[22]
+humidity=variables[23]
 gust=str(round(1.15077945*int(gust_kts[:-3])))
 temp_float=float(temp_str[:-7])
 heat_float=float(heatindex_str[:-7])
 chill_float=float(chill_str[:-7])
-tweet.write('Cndtns at ''{}''\n''Tmp: ''{}''°F''\n''DwPt: ''{}''°F'.format(time[:-1],temp_str[:-7],dew[:-7]))
+tweet.write('Wx at ''{}''\n''Tmp: ''{}''°F''\n''Hum: ''{}''%''\n''DwPt: ''{}''°F'.format(time[:-1],temp_str[:-7],humidity[:-1],dew[:-7]))
 if chill_float < temp_float:
-    tweet.write('\n''Wndchl: ''{}''°F'.format(chill_str[:-7]))
+    tweet.write('\n''Windchill: ''{}''°F'.format(chill_str[:-7]))
 if heat_float > temp_float:
-    tweet.write('\n''Ht indx: ''{}''°F'.format(heatindex_str[:-7]))
-tweet.write('\n''Wnd spd: ''{}'' mph (''{}' '°, ''{}'')''\n''Mx gst past hr: ''{}'' mph''\n''Baro: ''{}'' in Hg (''{}'' in Hg/hr)'.form$
+    tweet.write('\n''Ht ind: ''{}''°F'.format(heatindex_str[:-7]))
+tweet.write('\n''Wnd spd: ''{}'' mph (''{}' '°, ''{}'')''\n''Hrly gust: ''{}'' mph''\n''Baro: ''{}'' in Hg (''{}'' in Hg/hr)'.format(windsp[:-1],winddir[:-7],cardinal[:-1],gust[:-2],baro[:-5],barotrend[:-1]))
 if float(hourlyrain[:-1])>0:
-    tweet.write('\n''Hr rain: ''{}'' in'.format(hourlyrain[:-2]))
+    tweet.write('\n''Hrly rain: ''{}'' in'.format(hourlyrain[:-2]))
 if float(dailyrain[:-1])>0:
     tweet.write('\n''Dly rain: ''{}'' in'.format(dailyrain[:-1]))
 if float(solarrad[:-1])>0:
-    tweet.write('\n''Slr rd: ''{}'' W/m^2'.format(solarrad[:-1]))
+    tweet.write('\n''Slr rad: ''{}'' W/m^2'.format(solarrad[:-1]))
 if float(uv[:-1])>0:
-    tweet.write('\n''UV indx: ''{}'.format(uv[:-1]))
-tweet.write('\n''Soil moist: ''{}'' centibar''\n''Soil temp: ''{}''°F''\n''Pool temp: ''{}''°F'.format(soilmoist[:-1],soiltemp3[:-1],po$
+    tweet.write('\n''UV ind: ''{}'.format(uv[:-1]))
+tweet.write('\n''Soil moist: ''{}'' centibar''\n''Soil temp: ''{}''°F''\n''Pool temp: ''{}''°F'.format(soilmoist[:-1],soiltemp3[:-1],pooltemp[:-1]))
 f.close()
 tweet.close()
